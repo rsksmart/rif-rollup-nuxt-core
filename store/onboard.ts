08ab21306e2ef1196e238f93ac14f59091428fe8
Anni Piragauta <annipi@users.noreply.github.com>
2023-07-11 15:54:45 -0500

    release: rollup 232/wallet connect (#16)
    * feat: try to enable wallet connect
    
    * fix: appropriate loader error
    
    * feat(wallet): add 'wallet connect' option
    
    ---------
    
    Co-authored-by: Andrés Quijano <andres.quijano@iovlabs.org>
    Co-authored-by: Sergio <sergio.jara@iovlabs.org>

--- store/onboard.ts
+++ store/onboard.ts
@@ -14,7 +14,7 @@ import injectedModule from "@web3-onboard/injected-wallets";
 // import ledgerModule from "@web3-onboard/ledger";
 // import trezorModule from "@web3-onboard/trezor";
 // import dcentModule from "@web3-onboard/dcent";
-// import walletConnectModule from "@web3-onboard/walletconnect";
+import walletConnectModule from "@web3-onboard/walletconnect";
 import type { IConnector } from "@walletconnect/types";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
@@ -181,7 +181,7 @@ export const actions: ActionTree<OnboardState, OnboardState> = {
         theme: state.onboardTheme,
         wallets: [
           injectedModule(),
-          // walletConnectModule(),
+          walletConnectModule(),
           // ledgerModule(),
           // trezorModule({
           //   email: "support@zksync.io",
@@ -320,7 +320,7 @@ export const actions: ActionTree<OnboardState, OnboardState> = {
                 return reject(new Error("Unable to switch network"));
               }
 
-              if ((switchError as JsonRpcError<unknown>).code !== 4902 && (switchError as any).data?.originalError?.code !== 4902) {
+              if ((switchError as JsonRpcError).code !== 4902 && (switchError as any).data?.originalError?.code !== 4902) {
                 return reject(switchError);
               }
 
